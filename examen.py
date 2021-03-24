749cf93f5f86f66e2751fb685f7a03f187335a63
fbs11 <46707393+fbs11@users.noreply.github.com>
2021-03-24 10:56:19 +0100

    checkpoint 2

--- examen.py
+++ examen.py
@@ -1,17 +1,23 @@
 from libro import *
 
 
-f = open("fichero.txt", mode="rt", encoding="utf-8")
+f = open("fichero.txt", mode="wt", encoding="utf-8")
 palabras = ["boca", "pelo", "gracias", "nariz", "pez"]
-f.write("hola")
-f.write("gente")
-texto = f.read()
-print(texto)
+f.write("hola me llamo Fabio")
 f.close()
 
+fr = open("fichero.txt", mode="rt", encoding="utf-8")
 
-with open('fichero.txt', 'r') as palabras:
-    lineas = [linea.strip() for linea in palabras]
 
-for linea in lineas:
-    print(linea)
+def get_list(fr):
+    texto = fr.read()
+    separado = texto.split()
+    print(separado)
+    dic_palabras = {}
+    for i in separado:
+        len(i).append(dic_palabras)
+    print(dic_palabras)
+
+
+f.close()
+get_list(fr)
