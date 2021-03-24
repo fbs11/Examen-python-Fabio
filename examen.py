d938d276fc2cb0d5e3f47fca35229eda32451144
fbs11 <46707393+fbs11@users.noreply.github.com>
2021-03-24 12:11:01 +0100

    commit final

--- examen.py
+++ examen.py
@@ -1,3 +1,4 @@
+import unittest
 from libro import *
 from autor import *
 
@@ -35,16 +36,60 @@ def mas_antiguos(lista_libros, tiempo):
     res = []
 
     for i in libros:
-        if i.get_anyo() > 1900 and i.get_anyo() < 2021 and i.get_anyo():
-            res.append(i.get_autor())
-        else:
+        if i.get_anyo() <= tiempo:
+            res.append(i.get_titulo())
+        elif tiempo < 1900 or tiempo > 2021:
             raise ValueError("El año no es valido")
-        
+
     print(res)
     return res
 
 
+""" TESTS """
+
+
+class Pruebas(unittest.TestCase):
+    def test_muestra_todos(self):
+        l1 = Libro(autor="Juan", titulo="enfermedades", anyo=1970)
+        l2 = Libro(autor="Salvador", titulo="cocina", anyo=1980)
+        l3 = Libro(autor="Susana", titulo="fantasias", anyo=1990)
+        libros = [l1, l2, l3]
+
+        self.assertEqual(mas_antiguos(libros, 2000), "se añaden los 3")
+
+    def test_muestra_elmasantiguo(self):
+        l1 = Libro(autor="Juan", titulo="enfermedades", anyo=1970)
+        l2 = Libro(autor="Salvador", titulo="cocina", anyo=1980)
+        l3 = Libro(autor="Susana", titulo="fantasias", anyo=1990)
+        libros = [l1, l2, l3]
+
+        self.assertEqual(mas_antiguos(libros, 1970),
+                         "se añade el primero solo")
+
+    def test_muestra_nada(self):
+        l1 = Libro(autor="Juan", titulo="enfermedades", anyo=1970)
+        l2 = Libro(autor="Salvador", titulo="cocina", anyo=1980)
+        l3 = Libro(autor="Susana", titulo="fantasias", anyo=1990)
+        libros = [l1, l2, l3]
+
+        self.assertEqual(mas_antiguos(libros, 1930),
+                         "no muestra ninguno")
+
+
+class Suite(unittest.TestSuite):
+    def __init__(self):
+        super(Suite, self).__init__()
+        self.addTest(Pruebas('test_muestra_todos'))
+        self.addTest(Pruebas('test_muestra_elmasantiguo'))
+        self.addTest(Pruebas('test_muestra_nada'))
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    my_suite = Suite()
+    runner.run(my_suite)
+
 """ MAIN """
 
 get_list(fr)
-mas_antiguos(libros, 1980)
+mas_antiguos(libros, 1990)
--- tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from examen import *
-from libro import *
-from autor import *
-
-class Pruebas(unittest.TestCase):
-    
